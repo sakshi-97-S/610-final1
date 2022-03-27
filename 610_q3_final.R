cbcd7cd32c5875c2774a63a4e3977a1c5486f992
sakshi-97-S <69647872+sakshi-97-S@users.noreply.github.com>
2022-03-26 21:29:07 -0400

    Update 610_q3_final.R

--- 610_q3_final.R
+++ 610_q3_final.R
@@ -1,9 +1,25 @@
 
 
-eigen_fun = function(X,tol=.Machine$double.eps^0.75){
-  if(!isSymmetric(X)) stop("x is not symmetric")
+eigen_fun <- function(X,tol=.Machine$double.eps^0.75){
   d = ncol(X)
   U = diag(1,d,d,FALSE)
+  
+  Rlocal <- function(A){
+    d = ncol(X)
+    A <- C
+  B <- diag(1,d,d,FALSE)
+    for (i in 1:(d-1)) {
+      X <- matrix(rep(0,d),nrow = d)
+      X[i:d] <- R2[i:d,i]
+      V <- X
+      V[i] <- X[i]+norm(X,"F")*sign(X[i])
+      xlocal <- V/norm(V,"F")
+      A <- A-2*(xlocal%*%t(xlocal)%*%A)
+     B <- B-2*(xlocal%*%t(xlocal)%*%B)
+    }
+    return(list("B" = t(B), "C" = C))
+  }
+  
   while(TRUE){
     did_we_update = FALSE
     for(i in 1:(d-1)){
@@ -12,64 +28,77 @@ eigen_fun = function(X,tol=.Machine$double.eps^0.75){
         if(abs(X[i,j])>tol){
           # if so, get the Givens Rotation matrix R
           # and update X and U
-          R=diag(1,d,d,FALSE)
-          if(X[i,i]==X[j,j]){
-            sin=sin(pi/4)
-            cos=cos(pi/4)
-            R[i,i]=sin
-            R[i,j]=cos
-            R[j,j]=sin
-            R[j,i]=-1*cos
-            }else{
-            theta=1/2*atan(2*X[j,i]/(X[i,i]-X[j,j]))
-            sin=sin(theta)
-            cos=cos(theta)
-            R[i,i]=sin
-            R[i,j]=cos
-            R[j,j]=sin
-            R[j,i]=-1*cos
+          for (k in 1:1000){
+          update <- Rlocal(U)
+          Q = update$B
+          R = update$A
+          U = X %*% Q
           }
-            
-           X=R%*%X%*%t(R)
-           U=U%*%R
-            
-          
-          
           # track that in this sweep through we have done at least one update
-          did_we_update = TRUE
+          did_we_update = FALSE
         }
         
       }
     }
     if(!did_we_update) break
   }
-  values=diag(X)
-  sorted_values = sort(values,index.return=TRUE,decreasing = True)
-  values = sorted_values$X
-  vectors = U[,sorted_values$iX]
-  return(list(values=values, vectors=vectors))
+  
+  values = diag(R)
+  sorted_values = sort(values,index.return=TRUE,decreasing=TRUE)
+  values = sorted_values$x
+  vectors = Q[,sorted_values$ix]
+  return(list(values=values, vectors= -vectors))
+  
 }
-d=2
+set.seed(1234567890)
+d =3
 z = matrix(rnorm(d^d),d,d)
 X = z + t(z)
-set.seed(1234567890)
+# X
 eigen_fun(X)
+eigen(X)
 
-d=3
+test_that("this is d =3 value test",{
+  expect_equal(eigen_fun(X)$values,eigen(X)$values)
+}
+)
+
+
+set.seed(1234567890)
+d =2
 z = matrix(rnorm(d^d),d,d)
 X = z + t(z)
-set.seed(1234567890)
+# X
 eigen_fun(X)
+eigen(X)
 
-d=4
+test_that("this is d =2 value test",{
+  expect_equal(eigen_fun(X)$values,eigen(X)$values)
+}
+)
+
+set.seed(1234567890)
+d =4
 z = matrix(rnorm(d^d),d,d)
 X = z + t(z)
-set.seed(1234567890)
+# X
 eigen_fun(X)
+eigen(X)
 
-d=5
+test_that("this is d =4 value test",{
+  expect_equal(eigen_fun(X)$values,eigen(X)$values)
+}
+)
+
+set.seed(1234567890)
+d =5
 z = matrix(rnorm(d^d),d,d)
 X = z + t(z)
-set.seed(1234567890)
+# X
 eigen_fun(X)
+eigen(X)
 
+test_that("this is d =5 value test",{
+  expect_equal(eigen_fun(X)$values,eigen(X)$values)
+}
+)
